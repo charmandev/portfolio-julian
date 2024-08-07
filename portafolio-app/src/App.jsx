3fa1e601d0c7d685926264a75f2492ea0648c7b1
Juanliendo22 <liendojuan070@gmail.com>
2024-08-07 18:16:54 -0300

    mejoras en el fondo

--- portafolio-app/src/App.jsx
+++ portafolio-app/src/App.jsx
@@ -7,6 +7,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="background-container"></div>{" "}
+      {/* Contenedor del fondo SVG */}
       <Navbar />
       <div className="contenido">
         <div className="Texto">
