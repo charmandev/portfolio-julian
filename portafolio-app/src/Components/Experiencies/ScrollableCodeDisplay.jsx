72b6286c14dfc77578300abfcf673513b91da605
Juanliendo22 <liendojuan070@gmail.com>
2024-08-12 14:50:02 -0300

    acordeon

--- portafolio-app/src/Components/Experiencies/ScrollableCodeDisplay.jsx
+++ portafolio-app/src/Components/Experiencies/ScrollableCodeDisplay.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import CodeDisplay from "../CodeDisplay/CodeDisplay";
-import "./ScrollableCodeDisplay.css"; // Importa el archivo CSS
+import "./ScrollableCodeDisplay.css"; // Sigue usando el archivo CSS actual
 
 const ScrollableCodeDisplay = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
   return (
-    <div className="scrollable-container">
-      <div className="scrollable-content">
-        <div className="code-display-container">
-          <div className="code-item">
-            <CodeDisplay />
-          </div>
-          <div className="code-item">
-            <CodeDisplay />
-          </div>
-          <div className="code-item">
-            <CodeDisplay />
-          </div>
+    <div className="accordion-container">
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => toggleAccordion(0)}>
+          Experiencia 1{" "}
+        </div>
+        <div
+          className={`accordion-content ${activeIndex === 0 ? "active" : ""}`}
+        >
+          <CodeDisplay />
+        </div>
+      </div>
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => toggleAccordion(1)}>
+          Experiencia 2{" "}
+        </div>
+        <div
+          className={`accordion-content ${activeIndex === 1 ? "active" : ""}`}
+        >
+          <CodeDisplay />
+        </div>
+      </div>
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => toggleAccordion(2)}>
+          Experiencia 3{" "}
+        </div>
+        <div
+          className={`accordion-content ${activeIndex === 2 ? "active" : ""}`}
+        >
+          <CodeDisplay />
         </div>
       </div>
     </div>
