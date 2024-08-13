7bf1627561e6c38b1f6a4b4c6ffd63e27249b062
Juanliendo22 <liendojuan070@gmail.com>
2024-08-13 14:39:04 -0300

    mejoras en slide

--- portafolio-app/src/Components/Slide/Slide.jsx
+++ portafolio-app/src/Components/Slide/Slide.jsx
@@ -1,7 +1,7 @@
 // Slide.jsx
 import React from "react";
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
-import "./Slide.css"; // Importa el archivo CSS
+import "./Slide.css";
 
 const Slide = () => {
   return (
@@ -9,11 +9,26 @@ const Slide = () => {
       <h2>Skills</h2>
       <div className="icon-wrapper">
         <div className="icon-slider">
-          <FaHtml5 className="icon" />
-          <FaCss3Alt className="icon" />
-          <FaJs className="icon" />
-          <FaReact className="icon" />
-          <FaNodeJs className="icon" />
+          <div className="icon-item">
+            <FaHtml5 className="icon" />
+            <div className="icon-description">HTML5</div>
+          </div>
+          <div className="icon-item">
+            <FaCss3Alt className="icon" />
+            <div className="icon-description">CSS3</div>
+          </div>
+          <div className="icon-item">
+            <FaJs className="icon" />
+            <div className="icon-description">JavaScript</div>
+          </div>
+          <div className="icon-item">
+            <FaReact className="icon" />
+            <div className="icon-description">React</div>
+          </div>
+          <div className="icon-item">
+            <FaNodeJs className="icon" />
+            <div className="icon-description">Node.js</div>
+          </div>
         </div>
       </div>
     </div>
