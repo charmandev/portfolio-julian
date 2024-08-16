d13e3dff50836a669bfc6652a3cb261baf12f100
Juanliendo22 <liendojuan070@gmail.com>
2024-08-16 17:36:29 -0300

    mejoras

--- portafolio-app/src/Components/ContentSection/ContentSection.jsx
+++ portafolio-app/src/Components/ContentSection/ContentSection.jsx
@@ -39,8 +39,9 @@ const ContentSection = () => {
         <p>
           Hello, <br />{" "}
           <span className="text-celeste">I am Julian M. Gonzalez</span> <br />
-          <span className="text-rosa"> DevOps Engineer</span> <br /> And <br />
-          <span className="text-rosa">Fullstack Developer</span>
+          <span className="text-rosa"> DevOps Engineer</span> <br /> And
+          Fullstack
+          <span className="text-rosa"> Developer</span>
         </p>
 
         <div className="botones">
